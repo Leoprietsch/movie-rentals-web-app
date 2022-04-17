77d1b76a215ace786b6bf851d4169a2e3550f39a
leoprietsch <leo.prietsch@gmail.com>
2022-04-16 22:18:23 -0300

    config: allow Swagger outside of development environment

--- api/MovieRentals.Api/Startup.cs
+++ api/MovieRentals.Api/Startup.cs
@@ -40,12 +40,10 @@ namespace MovieRentals.Api
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Movie Rentals"));
-      }
+      if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Movie Rentals"));
 
       app.UseHttpsRedirection();
 
