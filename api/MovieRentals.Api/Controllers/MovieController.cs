1e01a0f7a95e0b008cbade1ab1c8096cc6b30ffb
leoprietsch <leo.prietsch@gmail.com>
2022-04-17 18:22:24 -0300

    feat: add movie controller

---
+++ api/MovieRentals.Api/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieRentals.Api.Models.Request;
+using MovieRentals.Domain;
+using MovieRentals.Service.Contracts;
+
+namespace MovieRentals.Api.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class MovieController : ControllerBase
+  {
+    private readonly IMovieService _clientService;
+
+    public MovieController(IMovieService clientService)
+      => _clientService = clientService;
+
+    [HttpGet]
+    public ActionResult<Movie[]> GetAll()
+      => Ok(_clientService.GetAll());
+  }
+}
