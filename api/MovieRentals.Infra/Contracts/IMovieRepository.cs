8383fdd1d04dc7c46b729cd3e6ed9e17d63cd654
leoprietsch <leo.prietsch@gmail.com>
2022-04-17 18:00:46 -0300

    feat: create movie repository

---
+++ api/MovieRentals.Infra/Contracts/IMovieRepository.cs
@@ -0,0 +1,11 @@
+using MovieRentals.Domain;
+
+namespace MovieRentals.Infra.Contracts
+{
+  public interface IMovieRepository
+  {
+    Movie[] GetAll();
+    Movie Get(int id);
+    Movie Create(Movie client);
+  }
+}
---
+++ api/MovieRentals.Infra/Repositories/MovieRepository.cs
@@ -0,0 +1,52 @@
+using System.Data;
+using System.Linq;
+using Dapper;
+using MovieRentals.Domain;
+using MovieRentals.Infra.Contracts;
+
+namespace MovieRentals.Infra.Repositories
+{
+  public class MovieRepository : IMovieRepository
+  {
+    private readonly IDbConnection _db;
+    public MovieRepository(IDbConnection db) => _db = db;
+
+    public Movie[] GetAll()
+    {
+      return _db.Query<Movie>(@"
+        SELECT
+          id,
+          titulo,
+          classificacaoIndicativa,
+          lancamento
+          FROM filme")?.ToArray();
+    }
+
+    public Movie Get(int id)
+    {
+      return _db.QueryFirstOrDefault<Movie>(@"
+        SELECT
+          id,
+          titulo,
+          classificacaoIndicativa,
+          lancamento
+        FROM filme
+        WHERE id = @Id", new { Id = id });
+    }
+
+    public Movie Create(Movie movie)
+    {
+      int id = _db.QuerySingle<int>(@"
+      INSERT INTO filme 
+        (Id, Titulo, ClassificacaoIndicativa, Lancamento)
+        VALUES 
+        (@Id, @Titulo, @ClassificacaoIndicativa, @Lancamento);
+      
+      SELECT LAST_INSERT_ID();", movie);
+
+      movie.Id = id;
+
+      return movie;
+    }
+  }
+}
