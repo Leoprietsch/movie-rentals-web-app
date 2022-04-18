10f1df7ee304473fce8ebd4ca83068f8f8f067b0
leoprietsch <leo.prietsch@gmail.com>
2022-04-18 15:28:50 -0300

    feat: add import movies button

--- app/pages/filmes.tsx
+++ app/pages/filmes.tsx
@@ -1,6 +1,7 @@
-import { Table } from "antd";
+import { Button, Table } from "antd";
 import { ColumnsType } from "antd/lib/table";
 import Title from "antd/lib/typography/Title";
+import { VideoCameraAddOutlined } from "@ant-design/icons";
 import React, { useEffect, useState } from "react";
 import Filme from "../entities/Filmes";
 import { getAll } from "../apiClients/filmesClient";
@@ -43,7 +44,17 @@ function Filmes() {
 
   return (
     <>
-      <Title>Filmes</Title>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Title>Filmes</Title>
+        <Button type="primary" size="large" icon={<VideoCameraAddOutlined />}>
+          Importar filmes
+        </Button>
+      </div>
       <Table columns={columns} dataSource={filmes} />
     </>
   );
