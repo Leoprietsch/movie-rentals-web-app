51447bf51e257c906a252d653cf3852a1f00f497
leoprietsch <leo.prietsch@gmail.com>
2022-04-18 18:22:04 -0300

    feat: add get by id and update api call implementation for clients

--- app/apiClients/clientesClient.ts
+++ app/apiClients/clientesClient.ts
@@ -2,3 +2,10 @@ import Cliente from "../entities/Cliente";
 import http from "../http-common";
 
 export const getAll = () => http.get<Array<Cliente>>("/client");
+
+export const get = (id: String) => http.get<Array<Cliente>>(`/client/${id}`);
+
+export const update = (id: String, cliente: Cliente) => {
+  console.log(cliente);
+  http.put<Array<Cliente>>(`/client/${id}`, cliente);
+};
