8bbb017a11141e5b4d621f709e53b7e8b6387054
leoprietsch <leo.prietsch@gmail.com>
2022-04-18 20:30:36 -0300

    refactor: remove console logs

--- app/apiClients/clientesClient.ts
+++ app/apiClients/clientesClient.ts
@@ -6,15 +6,11 @@ export const getAll = () => http.get<Array<Cliente>>("/client");
 export const get = (id: String | number) =>
   http.get<Array<Cliente>>(`/client/${id}`);
 
-export const create = (cliente: Cliente) => {
-  console.log(cliente);
-  return http.post<Array<Cliente>>(`/client`, cliente);
-};
+export const create = (cliente: Cliente) =>
+  http.post<Array<Cliente>>(`/client`, cliente);
 
-export const update = (id: String | number, cliente: Cliente) => {
-  console.log(cliente);
-  return http.put<Array<Cliente>>(`/client/${id}`, cliente);
-};
+export const update = (id: String | number, cliente: Cliente) =>
+  http.put<Array<Cliente>>(`/client/${id}`, cliente);
 
 export const exclude = (id: String | number) =>
   http.delete<Array<Cliente>>(`/client/${id}`);
