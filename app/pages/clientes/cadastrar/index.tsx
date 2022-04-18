bf14072a73ab070ddbf501e7f0928c36a6d6e96a
leoprietsch <leo.prietsch@gmail.com>
2022-04-18 13:18:49 -0300

    feat: register client page

---
+++ app/pages/clientes/cadastrar/index.tsx
@@ -0,0 +1,39 @@
+import Title from "antd/lib/typography/Title";
+import React from "react";
+import { Form, Input, Button, DatePicker } from "antd";
+import { Moment } from "moment";
+
+function CadastrarCliente() {
+  const onFinish = (values: any) => {
+    console.log(values);
+    const dataNascimento = values.dataNascimento as Moment;
+    console.log(dataNascimento.toDate());
+  };
+
+  return (
+    <>
+      <Title>Cadastro de cliente</Title>
+      <Form
+        wrapperCol={{ span: 6 }}
+        layout="vertical"
+        size={"middle"}
+        onFinish={onFinish}
+      >
+        <Form.Item required name="nome" label="Nome">
+          <Input />
+        </Form.Item>
+        <Form.Item name="cpf" required label="CPF">
+          <Input />
+        </Form.Item>
+        <Form.Item required name="dataNascimento" label="Data de Nascimento">
+          <DatePicker format={"DD/MM/yyyy"} />
+        </Form.Item>
+        <Form.Item>
+          <Button htmlType="submit">Salvar</Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+}
+
+export default CadastrarCliente;
